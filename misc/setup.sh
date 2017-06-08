ff29fcc6da6aa8d65f957e380fd302ac6f6cfc88
stebdus <steb81@gmail.com>
2017-06-08 10:29:36 +0200

    Create setup.sh

--- misc/setup.sh
+++ misc/setup.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 # Run as root and cd into misc dir before running!
 
-# Update and install docker stuff
-apt-get -y update
-apt-get -y install docker.io
-curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
 # Build webapp image
 cd ../webapp
 docker build -t webapp .
